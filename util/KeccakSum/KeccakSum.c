156ef69092794ff04cd818dd18027e5c7c52aa03
Gilles Van Assche <gva-github@noekeon.org>
2020-02-17 18:10:18 +0100

    Add support of ParallelHash to KeccakSum

--- util/KeccakSum/KeccakSum.c
+++ util/KeccakSum/KeccakSum.c
@@ -39,6 +39,7 @@ int hexencode(const void* data_buf, size_t dataLength, char* result, size_t resu
 #define bufferSize 65536
 #define algorithm_Keccak    0
 #define algorithm_K12       1
+#define algorithm_ParallelHash  2
 
 typedef struct {
     unsigned int algorithm;
@@ -46,11 +47,13 @@ typedef struct {
     unsigned int capacity;
     unsigned int hashbitlen;
     unsigned char delimitedSuffix;
+    unsigned int blockByteLen;
 } Specifications;
 
 typedef union {
     Keccak_HashInstance keccak;
     KangarooTwelve_Instance k12;
+    ParallelHash_Instance ph;
 } Instance;
 
 int hashInitialize(Instance *instance, const Specifications *specs)
@@ -71,6 +74,28 @@ int hashInitialize(Instance *instance, const Specifications *specs)
         else
             return 0;
     }
+    else if (specs->algorithm == algorithm_ParallelHash) {
+        if (specs->capacity == 256) {
+            if (ParallelHash128_Initialize(&instance->ph, specs->blockByteLen, specs->hashbitlen, "", 0)) {
+                printf("Incorrect ParallelHash128 parameters.\n");
+                return -1;
+            }
+            else
+                return 0;
+        }
+       else if (specs->capacity == 512) {
+            if (ParallelHash256_Initialize(&instance->ph, specs->blockByteLen, specs->hashbitlen, "", 0)) {
+                printf("Incorrect ParallelHash256 parameters.\n");
+                return -1;
+            }
+            else
+                return 0;
+        }
+        else {
+            printf("Incorrect ParallelHash parameters.\n");
+            return -1;
+        }
+    }
     else
         return -1;
 }
@@ -81,6 +106,10 @@ int hashUpdate(Instance *instance, const Specifications *specs, const unsigned c
         Keccak_HashUpdate(&instance->keccak, buffer, size*8);
     else if (specs->algorithm == algorithm_K12)
         KangarooTwelve_Update(&instance->k12, buffer, size);
+    else if ((specs->algorithm == algorithm_ParallelHash) && (specs->capacity == 256))
+        ParallelHash128_Update(&instance->ph, buffer, size*8);
+    else if ((specs->algorithm == algorithm_ParallelHash) && (specs->capacity == 512))
+        ParallelHash256_Update(&instance->ph, buffer, size*8);
     return 0;
 }
 
@@ -90,6 +119,10 @@ int hashFinal(Instance *instance, const Specifications *specs, unsigned char *bu
         Keccak_HashFinal(&instance->keccak, buffer);
     else if (specs->algorithm == algorithm_K12)
         KangarooTwelve_Final(&instance->k12, buffer, "", 0);
+    else if ((specs->algorithm == algorithm_ParallelHash) && (specs->capacity == 256))
+        ParallelHash128_Final(&instance->ph, buffer);
+    else if ((specs->algorithm == algorithm_ParallelHash) && (specs->capacity == 512))
+        ParallelHash256_Final(&instance->ph, buffer);
     return 0;
 }
 
@@ -186,6 +219,9 @@ void printHelp()
     printf("  --no-suffix                 Use 0x01 as delimited suffix (pure Keccak)\n");
     printf("  --ethereum                  Equivalent to --sha3-256 --no-suffix\n");
     printf("  --kangarootwelve or --k12   Use KangarooTwelve\n");
+    printf("  --ph128                     Use ParallelHash128\n");
+    printf("  --ph256                     Use ParallelHash256\n");
+    printf("  -B <block size in bytes>    ParallelHash's B parameter\n");
     printf("  --string <string>           String to hash\n");
     printf("  <file name>                 File to hash\n\n");
     printf("The options are processed in order.\nBy default, it uses SHAKE128 and base64 display.\n");
@@ -279,6 +315,36 @@ int process(int argc, char* argv[])
             specs.algorithm = algorithm_K12;
             specs.hashbitlen = 264;
             base64 = 1;
+        }
+        else if (strcmp("--ph128", argv[i]) == 0) {
+            specs.algorithm = algorithm_ParallelHash;
+            specs.capacity = 256;
+            specs.hashbitlen = 264;
+            specs.blockByteLen = 8192;
+            base64 = 1;
+        }
+        else if (strcmp("--ph256", argv[i]) == 0) {
+            specs.algorithm = algorithm_ParallelHash;
+            specs.capacity = 512;
+            specs.hashbitlen = 528;
+            specs.blockByteLen = 8192;
+            base64 = 1;
+        }
+        else if (strcmp("-B", argv[i]) == 0) {
+            int blockByteLen;
+            if ((i+1) >= argc) {
+                printf("Error: missing argument for -B\n");
+                return -1;
+            }
+            blockByteLen = 0;
+            if (sscanf(argv[i+1], "%d", &blockByteLen) && (blockByteLen > 0)) {
+                specs.blockByteLen = blockByteLen;
+                i++;
+            }
+            else {
+                printf("Error: argument for -B option must be a positive power of 2\n");
+                return -1;
+            }
         }
         else if (strcmp("--string", argv[i]) == 0) {
             if ((i+1) >= argc) {
