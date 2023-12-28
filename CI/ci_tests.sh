542d3835d59d7a9b275454e75151cf74f3da2ecd
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2023-12-28 11:59:08 +0100

    cover all cases to make sure we always test the high level code

--- CI/ci_tests.sh
+++ CI/ci_tests.sh
@@ -109,7 +109,11 @@ for t in generic32 generic32lc generic64 generic64lc SSSE3 AVX AVX2 AVX2noAsm AV
 					# Since the -march=native / -mtune=native toggles are used, we cannot rely on Qemu on
 					# CPUs that support AVX512. Hence we execute the test natively
 					echo "\n\n\n=========== Testing $t (compiled with $c compiler) for x86_64 (native test as AVX512 is not supported yet by Qemu)\n\n\n" | tee -a artifacts/CIlog.log
-					./bin/$t/UnitTests -p
+                    if [ "$t" = "generic64" ] && [ "$c" = "clang" ]; then
+                        ./bin/$t/UnitTests -a
+                    else
+                        ./bin/$t/UnitTests -p
+                    fi
 				else
 					echo "\n\n\n=========== Testing $t (compiled with $c compiler) for x86_64\n\n\n" | tee -a artifacts/CIlog.log
                     if [ "$t" = "generic64" ] && [ "$c" = "clang" ]; then
