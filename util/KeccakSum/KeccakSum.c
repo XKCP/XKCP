d10528e637796bb71fe386d77c0e000af29cb902
Gilles Van Assche <gva-github@noekeon.org>
2025-08-12 23:35:24 +0200

    Improve KeccakSum

--- util/KeccakSum/KeccakSum.c
+++ util/KeccakSum/KeccakSum.c
@@ -17,6 +17,7 @@ http://creativecommons.org/publicdomain/zero/1.0/
 #include "KeccakHash.h"
 #include "KangarooTwelve.h"
 #include "SP800-185.h"
+#include "TurboSHAKE.h"
 
 int hexencode(const void* data_buf, size_t dataLength, char* result, size_t resultSize)
 {
@@ -40,6 +41,7 @@ int hexencode(const void* data_buf, size_t dataLength, char* result, size_t resu
 #define algorithm_Keccak    0
 #define algorithm_K12       1
 #define algorithm_ParallelHash  2
+#define algorithm_TurboSHAKE    3
 
 typedef struct {
     unsigned int algorithm;
@@ -54,6 +56,7 @@ typedef union {
     Keccak_HashInstance keccak;
     KangarooTwelve_Instance k12;
     ParallelHash_Instance ph;
+    TurboSHAKE_Instance ts;
 } Instance;
 
 int hashInitialize(Instance *instance, const Specifications *specs)
@@ -96,6 +99,14 @@ int hashInitialize(Instance *instance, const Specifications *specs)
             return -1;
         }
     }
+    else if (specs->algorithm == algorithm_TurboSHAKE) {
+        if (TurboSHAKE_Initialize(&instance->ts, specs->capacity)) {
+            printf("Incorrect TurboSHAKE parameters.\n");
+            return -1;
+        }
+        else
+            return 0;
+    }
     else
         return -1;
 }
@@ -103,27 +114,36 @@ int hashInitialize(Instance *instance, const Specifications *specs)
 int hashUpdate(Instance *instance, const Specifications *specs, const unsigned char *buffer, size_t size)
 {
     if (specs->algorithm == algorithm_Keccak)
-        Keccak_HashUpdate(&instance->keccak, buffer, size*8);
+        return Keccak_HashUpdate(&instance->keccak, buffer, size*8);
     else if (specs->algorithm == algorithm_K12)
-        KangarooTwelve_Update(&instance->k12, buffer, size);
+        return KangarooTwelve_Update(&instance->k12, buffer, size);
     else if ((specs->algorithm == algorithm_ParallelHash) && (specs->capacity == 256))
-        ParallelHash128_Update(&instance->ph, buffer, size*8);
+        return ParallelHash128_Update(&instance->ph, buffer, size*8);
     else if ((specs->algorithm == algorithm_ParallelHash) && (specs->capacity == 512))
-        ParallelHash256_Update(&instance->ph, buffer, size*8);
-    return 0;
+        return ParallelHash256_Update(&instance->ph, buffer, size*8);
+    else if (specs->algorithm == algorithm_TurboSHAKE)
+        return TurboSHAKE_Absorb(&instance->ts, buffer, size);
+    else
+        return -1;
 }
 
 int hashFinal(Instance *instance, const Specifications *specs, unsigned char *buffer)
 {
     if (specs->algorithm == algorithm_Keccak)
-        Keccak_HashFinal(&instance->keccak, buffer);
+        return Keccak_HashFinal(&instance->keccak, buffer);
     else if (specs->algorithm == algorithm_K12)
-        KangarooTwelve_Final(&instance->k12, buffer, "", 0);
+        return KangarooTwelve_Final(&instance->k12, buffer, "", 0);
     else if ((specs->algorithm == algorithm_ParallelHash) && (specs->capacity == 256))
-        ParallelHash128_Final(&instance->ph, buffer);
+        return ParallelHash128_Final(&instance->ph, buffer);
     else if ((specs->algorithm == algorithm_ParallelHash) && (specs->capacity == 512))
-        ParallelHash256_Final(&instance->ph, buffer);
-    return 0;
+        return ParallelHash256_Final(&instance->ph, buffer);
+    else if (specs->algorithm == algorithm_TurboSHAKE) {
+        int code = TurboSHAKE_AbsorbDomainSeparationByte(&instance->ts, specs->delimitedSuffix);
+        code |= TurboSHAKE_Squeeze(&instance->ts, buffer, (specs->hashbitlen + 7)/8);
+        return code;
+    }
+    else
+        return -1;
 }
 
 int processFile(const char *fileName, const Specifications *specs, int base64)
@@ -210,29 +230,80 @@ int processString(const char *input, const Specifications *specs, int base64)
     return 0;
 }
 
+int processZeroes(size_t zeroes, const Specifications *specs, int base64)
+{
+    Instance instance;
+    unsigned char buffer[bufferSize];
+    char display[bufferSize*2+1];
+    size_t left, length;
+
+    if (specs->hashbitlen > (bufferSize*8)) {
+        printf("The requested digest length (%d bits) does not fit in the buffer.\n", specs->hashbitlen);
+        return -1;
+    }
+    if (hashInitialize(&instance, specs)) {
+        return -1;
+    }
+    memset(buffer, 0, bufferSize);
+    left = zeroes;
+    while(left > 0) {
+        length = (left <= bufferSize) ? left : bufferSize;
+        hashUpdate(&instance, specs, buffer, length);
+        left -= length;
+    }
+    hashFinal(&instance, specs, buffer);
+    if (base64) {
+        if (base64encode(buffer, (specs->hashbitlen+7)/8, display, bufferSize*2)) {
+            printf("Error while converting to base64.\n");
+            return -1;
+        }
+    }
+    else {
+        if (hexencode(buffer, (specs->hashbitlen+7)/8, display, bufferSize*2)) {
+            printf("Error while converting to hex.\n");
+            return -1;
+        }
+    }
+    printf("%s  ", display);
+    printf("`00`^%zu\n", zeroes);
+    return 0;
+}
+
 void printHelp()
 {
     printf("Usage: KeccakSum [options] [file names] [options] [file names] ...\n\n");
     printf("  --help or -h                To display this page\n");
+    printf("\nInput options:\n");
+    printf("  <file name>                 File to hash\n");
+    printf("  -                           Standard input to hash\n");
+    printf("  --string <string>           String to hash\n");
+    printf("  --zeroes <integer>          All-zero string with the given number of bytes\n");
+    printf("                              (for benchmarking purposes)\n");
+    printf("\nOutput options:\n");
     printf("  --base64                    Print in base64\n");
     printf("  --hex                       Print in hexadecimal\n");
-    printf("  --outputbits <numberOfBits> Number of bits as output\n");
-    printf("  --shake128                  Use SHAKE128\n");
-    printf("  --shake256                  Use SHAKE256\n");
-    printf("  --sha3-224                  Use SHA3-224\n");
-    printf("  --sha3-256                  Use SHA3-256\n");
-    printf("  --sha3-384                  Use SHA3-384\n");
-    printf("  --sha3-512                  Use SHA3-512\n");
+    printf("  --outputbits <integer>      Number of output bits\n");
+    printf("\nHashes and XOFs (roughly from fastest to slowest):\n");
+    printf("  --kangarootwelve or --k12   KT128\n");
+    printf("  --kt128                     KT128\n");
+    printf("  --kt256                     KT256\n");
+    printf("  --ph128                     ParallelHash128\n");
+    printf("  --ph256                     ParallelHash256\n");
+    printf("   -B <block size in bytes>   ParallelHash's B parameter\n");
+    printf("                              (default is 8192 bytes)\n");
+    printf("  --turboshake128 or --ts128  TurboSHAKE128\n");
+    printf("  --turboshake256 or --ts256  TurboSHAKE256\n");
+    printf("  --shake128                  SHAKE128\n");
+    printf("  --shake256                  SHAKE256\n");
+    printf("  --sha3-224                  SHA3-224\n");
+    printf("  --sha3-256                  SHA3-256\n");
+    printf("  --sha3-384                  SHA3-384\n");
+    printf("  --sha3-512                  SHA3-512\n");
     printf("  --no-suffix                 Use 0x01 as delimited suffix (pure Keccak)\n");
     printf("  --ethereum                  Equivalent to --sha3-256 --no-suffix\n");
-    printf("  --kangarootwelve or --k12   Use KangarooTwelve\n");
-    printf("  --ph128                     Use ParallelHash128\n");
-    printf("  --ph256                     Use ParallelHash256\n");
-    printf("  -B <block size in bytes>    ParallelHash's B parameter\n");
-    printf("  --string <string>           String to hash\n");
-    printf("  <file name>                 File to hash\n\n");
+    printf("\n");
     printf("The options are processed in order.\nBy default, it uses SHAKE128 and base64 display.\n");
-    printf("With no file names, or when file name is -, it reads standard input.\n"); 
+    printf("With no file names and no strings, it reads the standard input.\n");
 }
 
 int process(int argc, char* argv[])
@@ -240,7 +311,7 @@ int process(int argc, char* argv[])
     Specifications specs;
     int base64 = 1;
     int i, r;
-    int was_filename = 0;
+    int was_filename_or_string = 0;
     specs.algorithm = algorithm_Keccak;
     specs.rate = 1344;
     specs.capacity = 256;
@@ -271,44 +342,50 @@ int process(int argc, char* argv[])
         else if (strcmp("--shake128", argv[i]) == 0) {
             specs.rate = 1344;
             specs.capacity = 256;
-            specs.hashbitlen = 264;
+            specs.hashbitlen = (base64 ? 264 : 256);
             specs.delimitedSuffix = 0x1F;
-            base64 = 1;
         }
         else if (strcmp("--shake256", argv[i]) == 0) {
             specs.rate = 1088;
             specs.capacity = 512;
-            specs.hashbitlen = 528;
+            specs.hashbitlen = (base64 ? 528 : 512);
             specs.delimitedSuffix = 0x1F;
-            base64 = 1;
+        }
+        else if ((strcmp("--turboshake128", argv[i]) == 0) || (strcmp("--ts128", argv[i]) == 0)) {
+            specs.algorithm = algorithm_TurboSHAKE;
+            specs.capacity = 256;
+            specs.hashbitlen = (base64 ? 264 : 256);
+            specs.delimitedSuffix = 0x1F;
+        }
+        else if ((strcmp("--turboshake256", argv[i]) == 0) || (strcmp("--ts256", argv[i]) == 0)) {
+            specs.algorithm = algorithm_TurboSHAKE;
+            specs.capacity = 512;
+            specs.hashbitlen = (base64 ? 528 : 512);
+            specs.delimitedSuffix = 0x1F;
         }
         else if (strcmp("--sha3-224", argv[i]) == 0) {
             specs.rate = 1152;
             specs.capacity = 448;
             specs.hashbitlen = 224;
             specs.delimitedSuffix = 0x06;
-            base64 = 0;
         }
         else if (strcmp("--sha3-256", argv[i]) == 0) {
             specs.rate = 1088;
             specs.capacity = 512;
             specs.hashbitlen = 256;
             specs.delimitedSuffix = 0x06;
-            base64 = 0;
         }
         else if (strcmp("--sha3-384", argv[i]) == 0) {
             specs.rate = 832;
             specs.capacity = 768;
             specs.hashbitlen = 384;
             specs.delimitedSuffix = 0x06;
-            base64 = 0;
         }
         else if (strcmp("--sha3-512", argv[i]) == 0) {
             specs.rate = 576;
             specs.capacity = 1024;
             specs.hashbitlen = 512;
             specs.delimitedSuffix = 0x06;
-            base64 = 0;
         }
         else if (strcmp("--no-suffix", argv[i]) == 0) {
             specs.delimitedSuffix = 0x01;
@@ -318,26 +395,28 @@ int process(int argc, char* argv[])
             specs.capacity = 512;
             specs.hashbitlen = 256;
             specs.delimitedSuffix = 0x01;
-            base64 = 0;
         }
-        else if ((strcmp("--kangarootwelve", argv[i]) == 0) || (strcmp("--k12", argv[i]) == 0)) {
+        else if ((strcmp("--kangarootwelve", argv[i]) == 0) || (strcmp("--k12", argv[i]) == 0) || (strcmp("--kt128", argv[i]) == 0)) {
             specs.algorithm = algorithm_K12;
-            specs.hashbitlen = 264;
-            base64 = 1;
+            specs.capacity = 256;
+            specs.hashbitlen = (base64 ? 264 : 256);
+        }
+        else if (strcmp("--kt256", argv[i]) == 0) {
+            specs.algorithm = algorithm_K12;
+            specs.capacity = 512;
+            specs.hashbitlen = (base64 ? 528 : 512);
         }
         else if (strcmp("--ph128", argv[i]) == 0) {
             specs.algorithm = algorithm_ParallelHash;
             specs.capacity = 256;
-            specs.hashbitlen = 264;
+            specs.hashbitlen = (base64 ? 264 : 256);
             specs.blockByteLen = 8192;
-            base64 = 1;
         }
         else if (strcmp("--ph256", argv[i]) == 0) {
             specs.algorithm = algorithm_ParallelHash;
             specs.capacity = 512;
-            specs.hashbitlen = 528;
+            specs.hashbitlen = (base64 ? 528 : 512);
             specs.blockByteLen = 8192;
-            base64 = 1;
         }
         else if (strcmp("-B", argv[i]) == 0) {
             int blockByteLen;
@@ -361,7 +440,28 @@ int process(int argc, char* argv[])
                 return -1;
             }
             i++;
+            was_filename_or_string = 1;
             processString(argv[i], &specs, base64);
+        }
+        else if (strcmp("--help", argv[i]) == 0 || strcmp("-h", argv[i]) == 0) {
+            printHelp();
+            return 0;
+        }
+        else if (strcmp("--zeroes", argv[i]) == 0) {
+            if ((i+1) >= argc) {
+                printf("Error: missing argument for --zeroes\n");
+                return -1;
+            }
+            size_t zeroes = 0;
+            if (sscanf(argv[i+1], "%zu", &zeroes)) {
+                i++;
+            }
+            else {
+                printf("Error: incorrect argument for --zeroes\n");
+                return -1;
+            }
+            was_filename_or_string = 1;
+            processZeroes(zeroes, &specs, base64);
         }
         else if (strcmp("--help", argv[i]) == 0 || strcmp("-h", argv[i]) == 0) {
             printHelp();
@@ -370,17 +470,17 @@ int process(int argc, char* argv[])
         else {
             if (strlen(argv[i]) > 2) {
                 if ((argv[i][0] == '-') && (argv[i][1] == '-')) {
-                    printf("Unrecognized command '%s'\n", argv[i]);
+                    printf("Unrecognized option '%s'\n", argv[i]);
                     return -1;
                 }
             }
-            was_filename = 1;
+            was_filename_or_string = 1;
             r = processFile(argv[i], &specs, base64);
             if (r)
                 return r;
         }
     }
-    if (was_filename == 0) {
+    if (was_filename_or_string == 0) {
         r = processFile("-", &specs, base64);
         if (r)
             return r;
